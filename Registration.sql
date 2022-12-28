7e19e593ac368aa126434a338489d6c6b49cfe61
Aadhityaa <saadhityaa2001@gmail.com>
2022-12-28 09:45:19 +0530

    Registration MySql

---
+++ Registration.sql
@@ -0,0 +1,8 @@
+create database Aadhityaa;
+show databases;
+use Aadhityaa;
+create table registration (id int, name varchar(45), Age int , doj varchar(30), email varchar(45), gender varchar(20), contact long, 
+address varchar(50));
+show tables;
+select * from registration;
+
